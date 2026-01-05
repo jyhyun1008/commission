bce28a11620f06bdafec98b54e124fc3e8977cbf
jyhyun1008 <jayhyun1008@gmail.com>
2026-01-05 15:48:26 +0900

    Updates

---
+++ _nuxt/BC5w_lgS.js
@@ -0,0 +1,1 @@
+import{r as n}from"./Ukchpo3b.js";import a from"./DN2fTU7u.js";import{a0 as s,b as t,o as e,v as i,y as p}from"./DKVx5xku.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
